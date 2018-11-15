ee5811ecb7a2cf9c1c306ee82319ddd4427d9deb
at-rooshee <rushania@youcanbook.me>
2018-11-15 13:56:08 -0600

    Adding component, creating record of files

--- app/components/upload-images/component.js
+++ app/components/upload-images/component.js
@@ -1,13 +1,25 @@
 import Component from '@ember/component';
-import fetch from 'fetch';
+import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+  store: service(),
+
+  images: computed(function() {
+    return this.store.peekAll('image');
+  }),
+
   actions: {
     onChange(event) {
-      let { files } = event.target;
+      let { files } = event.target; // getting the files
+      files = Array.from(files); // As files are not an array we have to convert them
 
-      // TODO: Upload those files to the server (to `/analyze` endpoint)
-      // Use ember-fetch
+      files.forEach((file) => {
+        let { name, size } = file; // https://developer.mozilla.org/en-US/docs/Web/API/File
+        let url = URL.createObjectURL(file); // https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
+
+        this.store.createRecord('image', { name, size, url }) // https://emberjs.com/api/ember-data/3.5/classes/DS.Store/methods/createRecord?anchor=createRecord
+      });
     }
   }
 });
